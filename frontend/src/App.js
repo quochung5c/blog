d47bf6a694080d20de3803393dcb03af2d56f819
Maximuss Hùng <vidostartup@gmail.com>
2019-08-18 10:32:53 +0700

    test api v1.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,35 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import data from "./champion.json";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-
 // Application:
 // Tạo 1 app to-do list, cho phép nhập, xóa nhiệm vụ, đăng nhập tài khoản.
 
 export default function App() {
-  const [champion, setChampion] = useState("Hello");
+  const [champion, setChampion] = useState(0);
   useEffect(() => {
-    console.log("Hello");
-  }, []);
-  const handleChange = event => {
-    // setChampion(data.data[`${event.target.value}`]);
-    let result = data.data[`${event.target.value}`];
-    if (result === undefined) {
-      console.log("Waiting...");
-    } else {
-      setChampion(result.title);
-    }
-  };
-
-  // Tìm tên tướng => id => request champion=id
-
-  const handleSubmit = event => {
-    event.preventDefault();
     axios
       .get(
         `https://cors-anywhere.herokuapp.com/https://acs-garena.leagueoflegends.com/v1/players?name=Lulu%20C%C3%B4%20%C4%90%C6%A1n&region=VN`
@@ -44,9 +22,6 @@ export default function App() {
         );
       })
       .then(res => {
-        // let assists = res.data.games.games.map(item =>{
-        //   return item.participants[0].stats.assists
-        // });
         // data chung
         let data = res.data.games.games;
         // Lấy tổng assist
@@ -58,21 +33,32 @@ export default function App() {
         });
         console.log(`Số hỗ trợ mỗi game: ${assistPerGame}`);
         console.log(`Tổng lượng assist: ${totalAssists}`);
+        setChampion(totalAssists);
       });
     console.log("Loading...");
+  }, []);
+  const handleChange = event => {
+    // setChampion(data.data[`${event.target.value}`]);
+    let result = data.data[`${event.target.value}`];
+    if (result === undefined) {
+      console.log("Waiting...");
+    } else {
+      setChampion(result.title);
+    }
   };
+
+  // Tìm tên tướng => id => request champion=id
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Your champion"
-          onChange={handleChange}
-        />
-      </form>
-
-      {champion}
+      {champion === 0 ? (
+        <h4>Loading...</h4>
+      ) : (
+        <div>
+          <Typography variant="h6">Hoàn thành 100 assist với Lulu</Typography>
+          Hiện tại: <progress max="100" value={champion} /> - {champion} hỗ trợ
+        </div>
+      )}
     </div>
   );
 }
